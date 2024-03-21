8b191a452f7003c15a946a509bf314f7aaaa5484
leo <leonardoandrei41@gmail.com>
2024-03-21 18:34:52 -0300

    Implement drawing of topdown view in Core.js

--- src/Core.js
+++ src/Core.js
@@ -1,7 +1,10 @@
 class Core {
-  constructor() {
+  constructor(terrain, topdownCanvas) {
     this.updateListeners = [];
     this.update = this.update.bind(this);
+    this.terrain = terrain;
+    this.topdownCanvas = topdownCanvas;
+    this.topdownContext = this.topdownCanvas.getContext('2d');
     this.start();
   }
 
@@ -22,5 +25,27 @@ class Core {
     });
     this.lastFrameTime = currentTime;
     requestAnimationFrame(this.update);
+    this.drawTopdownView();
+  }
+
+  drawTopdownView() {
+    const tileWidth = Math.floor(this.topdownCanvas.width / this.terrain.width);
+    const tileHeight = Math.floor(this.topdownCanvas.height / this.terrain.height);
+    this.topdownContext.fillStyle = 'white';
+    this.topdownContext.fillRect(0, 0, this.topdownCanvas.width, this.topdownCanvas.height);
+    for (let row = 0; row < this.terrain.values.length; row += 1) {
+      const rowValues = this.terrain.values[row];
+      for (let column = 0; column < rowValues.length; column += 1) {
+        const value = rowValues[column];
+        if (value === '1') {
+          this.topdownContext.fillStyle = 'white';
+        } else {
+          this.topdownContext.fillStyle = 'black';
+        }
+        const xPosition = column * tileWidth;
+        const yPosition = row * tileHeight;
+        this.topdownContext.fillRect(xPosition, yPosition, xPosition + tileWidth, yPosition + tileHeight);
+      }
+    }
   }
 }
