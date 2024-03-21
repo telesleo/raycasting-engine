495f8c975b21c1bd34dbb1e2eb48d3c819008e56
leo <leonardoandrei41@gmail.com>
2024-03-21 19:50:38 -0300

    Implement entity drawing in Core.js

--- src/Core.js
+++ src/Core.js
@@ -1,8 +1,9 @@
 class Core {
-  constructor(terrain, topdownCanvas) {
+  constructor(terrain, topdownCanvas, entities = []) {
     this.updateListeners = [];
     this.update = this.update.bind(this);
     this.terrain = terrain;
+    this.entities = entities;
     this.topdownCanvas = topdownCanvas;
     this.topdownContext = this.topdownCanvas.getContext('2d');
     this.start();
@@ -29,10 +30,15 @@ class Core {
   }
 
   drawTopdownView() {
-    const tileWidth = Math.floor(this.topdownCanvas.width / this.terrain.width);
-    const tileHeight = Math.floor(this.topdownCanvas.height / this.terrain.height);
     this.topdownContext.fillStyle = 'white';
     this.topdownContext.fillRect(0, 0, this.topdownCanvas.width, this.topdownCanvas.height);
+    const tileWidth = Math.floor(this.topdownCanvas.width / this.terrain.width);
+    const tileHeight = Math.floor(this.topdownCanvas.height / this.terrain.height);
+    this.drawTopdownTerrain(tileWidth, tileHeight);
+    this.drawTopdownEntities(tileWidth, tileHeight);
+  }
+
+  drawTopdownTerrain(tileWidth, tileHeight) {
     for (let row = 0; row < this.terrain.values.length; row += 1) {
       const rowValues = this.terrain.values[row];
       for (let column = 0; column < rowValues.length; column += 1) {
@@ -47,5 +53,18 @@ class Core {
         this.topdownContext.fillRect(xPosition, yPosition, xPosition + tileWidth, yPosition + tileHeight);
       }
     }
+  }
+
+  drawTopdownEntities(tileWidth, tileHeight) {
+    this.entities.forEach((entity) => {
+      this.topdownContext.fillStyle = entity.color;
+      this.topdownContext.beginPath();
+      const radius = Math.floor(entity.size / 2 * tileWidth);
+      this.topdownContext.arc(
+        entity.position.x * tileWidth, entity.position.y * tileHeight, radius, 0, 2 * Math.PI
+      );
+      this.topdownContext.fillStyle = entity.color;
+      this.topdownContext.fill();
+    });
   }
 }
