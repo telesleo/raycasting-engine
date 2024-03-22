7fbfa18453eb6f3cc0f95b9b9619710ad7a5cd5b
leo <leonardoandrei41@gmail.com>
2024-03-21 21:46:59 -0300

    Add 'movement' property and 'update' method to Entity

--- src/Entity.js
+++ src/Entity.js
@@ -1,7 +1,11 @@
 class Entity {
-  constructor(xPosition = 0, yPosition = 0, color = 'gray', size = 10) {
+  constructor(xPosition = 0, yPosition = 0, color = 'gray', size = 10, speed = 5) {
     this.position = { x: xPosition, y: yPosition }
     this.color = color;
     this.size = size;
+    this.movementInput = { x: 0, y: 0 } 
+    this.speed = speed;
   }
+
+  update() { }
 }
