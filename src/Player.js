a0eae676edb62f9c5f103976a82da45c4e2f74ad
leo <leonardoandrei41@gmail.com>
2024-03-21 21:47:27 -0300

    Implement handling of movement input in Player.js

--- src/Player.js
+++ src/Player.js
@@ -1,5 +1,44 @@
 class Player extends Entity {
   constructor(xPosition = 0, yPosition = 0, color, size) {
     super(xPosition, yPosition, color, size);
+    this.movementInput = { x: 0, y: 0 }
+    this.onKeyDown = this.onKeyDown.bind(this);
+    this.onKeyUp = this.onKeyUp.bind(this);
+    document.addEventListener('keydown', this.onKeyDown);
+    document.addEventListener('keyup', this.onKeyUp);
+  }
+
+  onKeyDown(event) {
+    if (event.key === 's') {
+      this.movementInput.y = -1;
+    }
+    if (event.key === 'w') {
+      this.movementInput.y = 1;
+    }
+    if (event.key === 'a') {
+      this.movementInput.x = -1;
+    }
+    if (event.key === 'd') {
+      this.movementInput.x = 1;
+    }
+  }
+
+  onKeyUp(event) {
+    if (event.key === 's') {
+      this.movementInput.y = 0;
+    }
+    if (event.key === 'w') {
+      this.movementInput.y = 0;
+    }
+    if (event.key === 'a') {
+      this.movementInput.x = 0;
+    }
+    if (event.key === 'd') {
+      this.movementInput.x = 0;
+    }
+  }
+
+  update() {
+    this.move = normalize(this.movementInput.x, this.movementInput.y);
   }
 }
